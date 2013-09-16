a2047c11ed4b755d240139c4adce7220757db8ee
chethanvasishta <chethan.vasishta@gmail.com>
2013-09-16 13:05:58 +0530

    Added code to run multiple iterations of a test and compute mean and median for the output running times

--- pipelined1.cpp
+++ pipelined1.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <sys/time.h>
 #include <omp.h>
+#include <algorithm>
 using namespace std;
 
 #define SANITY_CHECK 1
@@ -30,6 +31,81 @@ bool isEqual(int **a, int **b, int n){
 
 }
 
+double doSequential(int **a, int n){
+
+    struct timeval before, after;
+
+    gettimeofday(&before, NULL);
+
+    for(int i = 0 ; i < n ; ++i){
+        for(int j = 0 ; j < n ; ++j){
+            a[i][j] = a[j][i];
+        }
+    }
+
+    gettimeofday(&after, NULL);
+
+    return time_diff(before, after);
+
+}
+
+double doParallelJ(int **a, int n){
+
+    struct timeval before, after;
+    
+    gettimeofday(&before, NULL);
+
+    for(int j = n-1 ; j >= 0 ; --j){
+#pragma omp parallel for
+        for(int i = 0 ; i < n ; ++i){
+            a[i][j] = a[j][i];
+        }
+    }
+
+    gettimeofday(&after, NULL);
+
+    return time_diff(before, after);
+}
+
+double doParallelI(int **a, int n){
+    
+    struct timeval before, after;
+
+    gettimeofday(&before, NULL);
+
+    for(int i = 0 ; i < n ; ++i){
+#pragma omp parallel for
+        for(int j = n-1 ; j >= 0 ; --j){
+            a[i][j] = a[j][i];
+        }
+    }
+    gettimeofday(&after, NULL);
+
+    return time_diff(before, after);
+}
+
+
+void copy(int **src, int** dest, int n){
+    for(int i = 0 ; i < n ; ++i){
+        for(int j = 0 ; j < n ; ++j){
+            dest[i][j] = src[i][j];
+        }
+    }
+}
+
+double average(double *x, int n){
+    double avg = 0.0f;
+    for(int i = 0 ; i < n ; ++i)
+        avg += x[i];
+    return avg/n;
+}
+
+double median(double *x, int n){
+    sort(x, x+n);
+    return x[n/2];
+}
+
+
 int main(){
     int n;
     int **dist, **distCopy, **distPar;
@@ -47,79 +123,50 @@ int main(){
 		distCopy[i] = new int[n];
 		distPar[i] = new int[n];
 		for(int j = 0 ; j < n ; ++j){
-			dist[i][j] = rand()%100;
-            distCopy[i][j] = dist[i][j];
-            distPar[i][j] = dist[i][j];
+			distCopy[i][j] = rand()%100;
         }
 	}
 
     cout << "done initializing ... " << endl;
-   
+  
+    const int iterations = 25;
+
     //non-parallel  
-
-    struct timeval before, after;
-
-    gettimeofday(&before, NULL);
-
-    for(int i = 0 ; i < n ; ++i){
-        for(int j = 0 ; j < n ; ++j){
-            dist[i][j] = dist[j][i];
-        }
+   
+    double *seqTime = new double[iterations];
+    for(int i = 0 ; i < iterations ; ++i){
+        copy(distCopy, dist, n);
+        seqTime[i] = doSequential(dist, n);
     }
-
-    gettimeofday(&after, NULL);
-
-    cout << "Non - parallel execution took " << time_diff(before, after) << " milliseconds " << endl; 
-
+    cout << "Non - parallel execution : Mean - " << average(seqTime, iterations) << " milliseconds, Median -  " << median(seqTime, iterations) << " milliseconds " <<endl;
+    
     //parallel with j
     
-    gettimeofday(&before, NULL);
-   
-    for(int j = n-1 ; j >= 0 ; --j){
-#pragma omp parallel for
-        for(int i = 0 ; i < n ; ++i){
-            distPar[i][j] = distPar[j][i];
-        }
+    double *parJTime = new double[iterations];
+    for(int i = 0 ; i < iterations ; ++i){
+        copy(distCopy, distPar, n);
+        parJTime[i] = doParallelJ(distPar, n);    
     }
-
-    gettimeofday(&after, NULL);
-    
-    cout << "Parallel execution along j took " << time_diff(before, after) << " milliseconds " << endl;
-
-#if SANITY_CHECK
-   
-    cout << "Parallelization is " << (isEqual(dist, distPar,n) ? "sane" : "insane") << endl;
-    
-#endif //SANITY_CHECK
-
-    //copy back
-    
-    for(int i = 0 ; i < n ; ++i){
-        for(int j = 0 ; j < n ; ++j){
-            distPar[i][j] = distCopy[i][j];
-        }
-    }
-
-    //parallel with i
-    //Might be slower than j because of caching issues
-    
-    gettimeofday(&before, NULL);
-   
-    for(int i = 0 ; i < n ; ++i){
-#pragma omp parallel for
-        for(int j = n-1 ; j >= 0 ; --j){
-            distPar[i][j] = distPar[j][i];
-        }
-    }
-
-    gettimeofday(&after, NULL);
-    
-    cout << "Parallel execution along i took " << time_diff(before, after) << " milliseconds " << endl;
+    cout << "Parallel execution with j: Mean - " << average(parJTime, iterations) << " milliseconds, Median -  " << median(parJTime, iterations) << " milliseconds " <<endl;
 
 #if SANITY_CHECK
    
     cout << "Parallelization is " << (isEqual(dist, distPar, n) ? "sane" : "insane") << endl;
     
+#endif //SANITY_CHECK
+
+    //parallel with i
+    //Might be slower than j because of caching issues
+    
+    double *parITime = new double[iterations];
+    for(int i = 0 ; i < iterations ; ++i){
+        copy(distCopy, distPar, n);
+        parITime[i] = doParallelI(distPar, n);    
+    }
+    cout << "Parallel execution with i: Mean - " << average(parITime, iterations) << " milliseconds, Median -  " << median(parITime, iterations) << " milliseconds " <<endl;
+   
+#if SANITY_CHECK
+    cout << "Parallelization is " << (isEqual(dist, distPar, n) ? "sane" : "insane") << endl;
 #endif //SANITY_CHECK
 
     return 0;
